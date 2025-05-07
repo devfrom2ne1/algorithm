23a4a5c3992520a3c851d1d7e863eee027314d3f
devfrom2ne1 <124670711+devfrom2ne1@users.noreply.github.com>
2025-05-07 12:43:11 +0900

    Create 04_자연수_뒤집어_배열로_만들기.java

---
+++ programmers/04_String/04_자연수_뒤집어_배열로_만들기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.math.*;
+
+class Solution {
+    public int[] solution(long n) {
+        int[] answer = {};
+        
+        String str = String.valueOf(n);
+        int length = str.length();
+        answer = new int[length];
+        int cnt = 0;
+        
+        for(int i=length-1; i>=0; i--){
+            char ch = str.charAt(i);
+            
+            // 아스키코드를 int로 변환 (3가지 방법)
+            // (1) int k = (int) ch - '0'; 
+            // (2) int k = Character.getNumericValue(ch);
+            // (3) int k = Integer.parseInt( String.valueOf(ch) );
+            int k = Integer.parseInt( String.valueOf(ch) );
+            answer[cnt] = k;
+            cnt++;
+        }
+        
+        return answer;
+    }
+}
